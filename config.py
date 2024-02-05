323e944e165de1fe3e849816f749ed04db95ffb5
mojoee <47809126+mojoee@users.noreply.github.com>
2024-02-05 11:20:29 +0800

    added embeddings with huggingface

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 EMBEDDING_MODEL = "text-embedding-ada-002"
 PG_COLLECTION_NAME = "pdf_rag"
+EMBEDDING_MODEL_NAME = "BAAI/bge-small-en"
--- importer/load_and_process.py
+++ importer/load_and_process.py
@@ -2,9 +2,10 @@ import os
 import sys
 sys.path.insert(1, '/Users/ma012/Documents/Morit/10_ResearchProjects/08_ESGReader/Prototype')
 from dotenv import load_dotenv
-from config import EMBEDDING_MODEL, PG_COLLECTION_NAME
+from config import EMBEDDING_MODEL, PG_COLLECTION_NAME, EMBEDDING_MODEL_NAME
 from langchain_community.document_loaders import DirectoryLoader, UnstructuredPDFLoader
 from langchain_community.vectorstores import PGVector
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain_openai import OpenAIEmbeddings
 from langchain_experimental.text_splitter import SemanticChunker
 
@@ -23,12 +24,20 @@ loader = DirectoryLoader(
 docs = loader.load()
 
 
-embeddings = OpenAIEmbeddings(
-    model=EMBEDDING_MODEL
-)
+if EMBEDDING_MODEL_NAME == "BAAI/bge-small-en":
+    model_kwargs = {"device": "cuda"}
+    encode_kwargs = {"normalize_embeddings": True}
+    embeddings = HuggingFaceBgeEmbeddings(
+        model_name=EMBEDDING_MODEL_NAME, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
+    )
+
+elif EMBEDDING_MODEL_NAME == "openAI":
+    embeddings = OpenAIEmbeddings(
+        model=EMBEDDING_MODEL
+    )
 
 text_splitter = SemanticChunker(
-    embeddings=OpenAIEmbeddings()
+    embeddings=embeddings
 )
 
 chunks = text_splitter.split_documents(docs)
